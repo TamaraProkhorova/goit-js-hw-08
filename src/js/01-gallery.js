96593dc37e4bb8649162d51f8643e3af58788588
TamaraProkhorova <99213837+TamaraProkhorova@users.noreply.github.com>
2023-01-27 22:10:56 +0200

    Імпортує бібліотеку simple-lightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
-// Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css"
 import { galleryItems } from './gallery-items';
-// Change code below this line
-
 console.log(galleryItems);
+
+const galleryListRef = document.querySelector(".gallery");
+
+function createImagesCardsMarkup(images) {
+  return images
+  .map
+  ((image) =>
+      `<div class="gallery__item">
+       <a class="gallery__link" href="${image.original}">
+       <img
+         class="gallery__image"
+         src="${image.preview}"
+         data-source="${image.original}"
+         alt="${image.description}"
+       />
+     </a>
+   </div>`
+  )
+  .join(" ")
+}
+
+const galaryMarkup = createImagesCardsMarkup(galleryItems)
+galleryListRef.insertAdjacentHTML("beforeend", galaryMarkup);
