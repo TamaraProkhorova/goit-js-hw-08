d0734c124043befb896256a926e2925b6a35a304
TamaraProkhorova <99213837+TamaraProkhorova@users.noreply.github.com>
2023-01-29 00:20:21 +0200

    Вирішує 1шу задачу

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,25 +3,31 @@ import "simplelightbox/dist/simple-lightbox.min.css"
 import { galleryItems } from './gallery-items';
 console.log(galleryItems);
 
+console.log(galleryItems);
+
 const galleryListRef = document.querySelector(".gallery");
 
 function createImagesCardsMarkup(images) {
   return images
   .map
   ((image) =>
-      `<div class="gallery__item">
-       <a class="gallery__link" href="${image.original}">
+      `<a class="gallery__item" href="${image.original}">
        <img
          class="gallery__image"
          src="${image.preview}"
-         data-source="${image.original}"
          alt="${image.description}"
        />
-     </a>
-   </div>`
+     </a>`
   )
   .join(" ")
 }
 
 const galaryMarkup = createImagesCardsMarkup(galleryItems)
 galleryListRef.insertAdjacentHTML("beforeend", galaryMarkup);
+
+new SimpleLightbox('.gallery a', { 
+    captionsData: "alt",
+    captionDelay: 250,
+})
+
+console.log(galleryListRef)
