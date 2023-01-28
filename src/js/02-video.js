ea5104d06b51132f22d73825191e097ed5e96d0f
TamaraProkhorova <99213837+TamaraProkhorova@users.noreply.github.com>
2023-01-29 00:20:49 +0200

    Вирішує 2гу задачу без throttle

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const STORAGE_KEY = "videoplayer-current-time";
+// const throttled = new throttle(iframe);
 
+player.on('timeupdate', onPlay);
+
+function onPlay({seconds}) {
+    console.log(seconds);
+
+    localStorage.setItem(STORAGE_KEY, seconds);
+}
+// throttled.throttle(onPlay, 60);
+
+player.setCurrentTime(localStorage.getItem(STORAGE_KEY)|| 0);
