c3118cbc320aada8e0ab2dd6eb1c74070f818bdb
TamaraProkhorova <99213837+TamaraProkhorova@users.noreply.github.com>
2023-01-29 18:08:26 +0200

    Вирішує 2ге завдання

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,15 +3,13 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 const STORAGE_KEY = "videoplayer-current-time";
-// const throttled = new throttle(iframe);
 
-player.on('timeupdate', onPlay);
+player.on('timeupdate', throttle(onPlay,1000));
 
 function onPlay({seconds}) {
     console.log(seconds);
 
     localStorage.setItem(STORAGE_KEY, seconds);
 }
-// throttled.throttle(onPlay, 60);
 
 player.setCurrentTime(localStorage.getItem(STORAGE_KEY)|| 0);
