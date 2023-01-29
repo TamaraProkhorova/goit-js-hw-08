3f3a782b55779a9c0e4ff3cf2fbf6a234c92d093
TamaraProkhorova <99213837+TamaraProkhorova@users.noreply.github.com>
2023-01-30 00:06:24 +0200

    Вирішує 3тю задачу. Немає 2го пункту

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,27 @@
+import throttle from 'lodash.throttle';
+const form = document.querySelector('form');
+const STORAGE_KEY = "feedback-form-state";
+
+const formData = {};
+populateForm();
+
+form.addEventListener('input', throttle(event =>{
+    formData[event.target.name] = event.target.value;
+// console.log(formData)
+localStorage.setItem(STORAGE_KEY, JSON.stringify(formData), 500)
+}))
+
+form.addEventListener('submit', onFormSubmit)
+function onFormSubmit (event){
+    event.preventDefault();
+    console.log(formData);
+    event.currentTarget.reset();
+}
+
+function populateForm(){
+    const savedData = JSON.parse(localStorage.setItem(STORAGE_KEY));
+
+     console.log(savedData)
+}
+
 
