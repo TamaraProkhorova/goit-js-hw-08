dc8dab056b718be51a5ea2379c21f3b1cd5e8598
TamaraProkhorova <99213837+TamaraProkhorova@users.noreply.github.com>
2023-01-30 20:54:13 +0200

    Вирішує 3тю задачу

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,31 +1,38 @@
 import throttle from 'lodash.throttle';
-const form = document.querySelector('form');
 
-const STORAGE_KEY = "feedback-form-state";
-
-
+const STORAGE_KEY = 'feedback-form-state';
 let formData = {};
+
+form = document.querySelector('form'), 
+input = document.querySelector('input'), 
+textarea = document.querySelector('textarea'),
+
+form.addEventListener('input', throttle(storageFormData, 500));
+form.addEventListener('submit', onFormSubmit);
+
 populateForm();
 
-form.addEventListener('input', throttle(event =>{
-    formData[event.target.name] = event.target.value;
-// console.log(formData)
-localStorage.setItem(STORAGE_KEY, JSON.stringify(formData), 500)
-}))
-
-form.addEventListener('submit', onFormSubmit);
-function onFormSubmit (event){
-    event.preventDefault();
-    console.log(formData);
-    event.currentTarget.reset();
+function storageFormData(e) { 
+    formData[e.target.name] = e.target.value; 
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
 
-function populateForm(){
-    const savedData = JSON.parse(localStorage.setItem(STORAGE_KEY, formData));
-    console.log(savedData)
-//    if (localStorage.getItem(STORAGE_KEY))
-//     input.value = savedData.email || '';
-//     textarea.value = savedData.message || '';
+function onFormSubmit(e) { 
+    e.preventDefault(); 
+        
+    const savedDatas = JSON.parse(localStorage.getItem(STORAGE_KEY)); 
+    console.log(savedDatas); 
+    
+    e.currentTarget.reset(); 
+    localStorage.removeItem(STORAGE_KEY); 
+    formData = {};
+}
+
+function populateForm() { 
+    if (localStorage.getItem(STORAGE_KEY)) {  
+        input.value = JSON.parse(localStorage.getItem(STORAGE_KEY)).email || ''; 
+        textarea.value = JSON.parse(localStorage.getItem(STORAGE_KEY)).message || ''; 
+        }
     }
 
 
